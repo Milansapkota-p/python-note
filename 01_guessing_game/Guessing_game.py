c7633141f3ef022acabe640594705cb92b4d1541
Milansapkota-p <milansapkotap@gmail.com>
2025-12-19 19:00:58 +0545

    updating the code

--- 01_guessing_game/Guessing_game.py
+++ 01_guessing_game/Guessing_game.py
@@ -1,1 +1,13 @@
-print("hello world")
+import random
+#randint is function which gernerate random integer value
+target=random.randint(1,100)
+while(1):
+    userChoice=int(input("Guess the target:"))
+    if(userChoice==target):
+        print("congratulation your guess is correct")
+        break
+    elif(userChoice>target):
+        print("Your number was big .Take a smaller guess")
+    else:
+        print("your number was small .Take a bigger guess")
+print("-----Game over-----")
