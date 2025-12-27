e92d2442ea1715708a9b5b48a5b9aa40cc0e81e6
Milansapkota-p <milansapkotap@gmail.com>
2025-12-27 16:54:44 +0545

    feat:Adding backup feature

---
+++ 06_Student Management System/back_up.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import datetime
+source="C:\\Users\\D E L L\\Documents\\git\\python-note\\06_Student Management System\\records"
+destination="C:\\Users\\D E L L\\Documents\\git\\python-note\\06_Student Management System\\backup"
+def copy_file_to_dist(src,dest):
+    today=datetime.date.today()
+    dest_dir=os.path.join(destination,str(today))
+    print(dest_dir)
+    try:
+        shutil.copytree(src,dest_dir)
+        print(f"file is copy to {dest_dir}")
+    except FileExistsError:
+        print(f"file already exist in: {dest}")
+def backup():
+        copy_file_to_dist(source,destination)
+if __name__=="__main__":
+    backup()
