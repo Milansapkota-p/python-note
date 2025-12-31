9cd772b8e4698558bd885611a53678c64962432e
Milansapkota-p <milansapkotap@gmail.com>
2025-12-31 23:17:52 +0545

    feat:view book and search book

--- Library Management System/main.py
+++ Library Management System/main.py
@@ -2,33 +2,55 @@ import json
 class library_info:
     def __init__(self):
         pass
+    def get_int(self,message):
+        while True:
+            try:
+                return int(input(message))
+            except ValueError:
+                print("Error! Enter numbers only!!")
     def add_book(self):
         try:
             with open("library_data","r") as f:
                 lab_info=json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             lab_info={}
-        book_id=int(input("Enter book id:"))
-        for id in lab_info.keys():
-            if(int(id)==book_id):
-                print(f"{book_id} id is already exsit please make new one!!")
-                return
+        while True:
+            found=False
+            book_id=self.get_int("Enter book id:")
+            for id in lab_info.keys():
+                if(int(id)==book_id):
+                    found=True
+                    print(f"{book_id} id is already exsit please make new one!!")
+            if found==False:
+                break
         book_name=input("Enter title of book:")
         author=input(f"Enter author name of {book_name} book:")
         catogory=input(f"Enter catogory of {book_name} book:")
-        t_copies=int(input(f"Enter total copies of {book_name}:"))
-        avil_copies=int(input(f"Enter total avilable copies:"))
+        t_copies=self.get_int(f"Enter total copies of {book_name}:")
+        avil_copies=self.get_int(f"Enter total avilable copies:")
         lab_info[book_id]={"book name":book_name,"author":author,"catogory":catogory,
                            "total copies":t_copies,"avilable copies":avil_copies}
         with open("library_data","w") as f:
             json.dump(lab_info,f,indent=2)
-            print(lab_info)
-
-
+            print("Data is successfully added!!")
     def view_book(self):
-        pass
+        try:
+            with open("library_data","r") as f:
+                lab_info=json.load(f)
+                for key in lab_info.keys():
+                    print(f"Book Id={key}:{lab_info.get(key)}")
+        except (FileNotFoundError, json.JSONDecodeError):
+            print("file is not created!!")
     def search_book(self):
-        pass
+        try:
+            with open("library_data","r") as f:
+                lab_info=json.load(f)
+                book_id=self.get_int("Enter book id:")
+                for key in lab_info.keys():
+                    if(book_id==int(key)):
+                        print(f"Book id={key}:{lab_info.get(key)}")
+        except (FileNotFoundError, json.JSONDecodeError):
+            print("file is not created!!")
     def update_book(self):
         pass
     def delete_book(self):
